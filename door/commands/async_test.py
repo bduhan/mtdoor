4b2c7eb6927b11c67389c8bfd27f6c92feb20160
bduhan <brian.duhan@gmail.com>
2024-10-26 18:24:42 -0500

    Update async_test as example.

--- door/commands/async_test.py
+++ door/commands/async_test.py
@@ -1,26 +1,24 @@
 import time
-import asyncio
-
-from rich.pretty import pprint
 
 from loguru import logger as log
 
 from .base_command import BaseCommand
 
+
 class AsyncTest(BaseCommand):
+    """
+    Demonstrates a background thread.
+    """
     command = "async"
     description = "test command"
+    help = "Run a thread, sleep for 10 seconds, reply."
 
     def invoke(self, msg: str, node: str) -> None:
         self.run_in_thread(self.wait_in_thread, msg, node)
 
     def wait_in_thread(self, msg, node):
-        print(f"thread_method sleeping in response to '{msg}'..")
-        pprint(self.get_node(node))
-        time.sleep(5)
-        print("thread_method done")
+        log.debug(self.get_node(node))
+        log.debug(f"thread_method sleeping..")
+        time.sleep(10)
+        log.debug("thread_method done")
         self.send_dm("waited", node)
-
-
-    # async def waiter(self, msg: str, node: str) -> None:
-        
