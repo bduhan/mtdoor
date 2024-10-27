59e25a33315ae767582096897a9d147cb4e12b47
bduhan <brian.duhan@gmail.com>
2024-10-26 19:03:38 -0500

    Clean and tweak help.

--- door/commands/me.py
+++ door/commands/me.py
@@ -1,16 +1,13 @@
-from loguru import logger as log
-
 from .base_command import BaseCommand
+
 
 class AboutMe(BaseCommand):
     command = "me"
     description = "about you"
-    help = "'me', 'me pos', 'me metrics'"
+    help = "'Last heard' time is in UTC."
 
     def invoke(self, msg: str, node: str) -> str:
         u = self.get_node(node)
-        log.debug(msg)
-
 
         reply = f"""Name: {u.user.longName} ({u.user.shortName})
 Last heard: {u.last_heard.strftime("%Y-%m-%d %H:%M")}
@@ -18,11 +15,11 @@ SNR: {u.snr}
 Hops: {u.hopsAway}"""
 
         if u.position:
-             reply += f"""
+            reply += f"""
 Position: {u.position.latitude}, {u.position.longitude}, {u.position.altitude}
 """
         if u.deviceMetrics:
-             reply += f"""
+            reply += f"""
 Device Metrics:
 - battery: {u.deviceMetrics.batteryLevel}% {u.deviceMetrics.voltage} volts
 - utilization: {u.deviceMetrics.channelUtilization} ({u.deviceMetrics.airUtilTx:.2})
