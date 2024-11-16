31dc5955c24beb1264f899d6aab1a3dc0c565d8c
Mike Krzywonski <mike@krzywonski.me>
2024-11-16 13:38:20 -0600

    Fixed bugs when node attributes are None or when response exceeds max message length. Added listing by short name or node ID to list more nodes in limited message size.

--- door/commands/node.py
+++ door/commands/node.py
@@ -4,14 +4,22 @@ from . import BaseCommand
 from ..models import NodeInfo
 
 
-def format_node_list(nodes: list[NodeInfo]) -> str:
+def format_node_list(nodes: list[NodeInfo], msg: str) -> str:
     """
     output suitable for a list of nodes
     """
     response = ""
     n: NodeInfo
     for n in nodes:
-        r = f"{n.user.id}: {n.user.longName} ({n.user.shortName})\n"
+        if msg == "sn":    
+            # Listing only short names will maximize the number of nodes in response
+            r = f"{n.user.shortName}\n"    
+        elif msg == "ln":
+            # This gives more info but will limit the # of nodes in response
+            r = f"{n.user.id}: {n.user.longName} ({n.user.shortName})\n"
+        else:    
+            # List only nodeId by default 
+            r = f"{n.user.id}\n"
 
         if len(response) + len(r) > 200:
             break
@@ -26,28 +34,54 @@ def format_node_detail(n: NodeInfo) -> str:
     output for detail of a node
     """
 
-    reply = f"""Name: {n.user.longName} ({n.user.shortName})
-Last heard {n.last_heard.strftime("%Y-%m-%d %H:%M")}
-SNR {n.snr}, Hops {n.hopsAway}"""
-
+    reply = f"ID: {n.user.id} "
+    # Make sure each item is not None before trying to add it
+    if n.user.longName and n.user.shortName:
+        reply += f"Name: {n.user.longName} ({n.user.shortName})\n"
+    if n.last_heard:
+        reply += f"Last heard {n.last_heard.strftime('%Y-%m-%d %H:%M')}\n"
+    if n.snr != None and n.hopsAway != None:
+        reply += f"SNR {n.snr}, Hops {n.hopsAway}\n"
     if n.position:
-        reply += f"""
-Pos.: {n.position.latitude:.7}, {n.position.longitude:.8}, {n.position.altitude}
-"""
-
+        if n.position.latitude and n.position.longitude:
+            reply += f"Pos.: {n.position.latitude:.7}, {n.position.longitude:.8}"
+        if n.position.altitude:
+            reply += f", {n.position.altitude}\n"
+        else:
+            reply += "\n"
     if n.deviceMetrics:
-        reply += f"""
-Batt.: {n.deviceMetrics.batteryLevel}% {n.deviceMetrics.voltage:.3}V
-Util.: {n.deviceMetrics.channelUtilization} ch, {n.deviceMetrics.airUtilTx:.2} air
-Up: {n.deviceMetrics.uptimeSeconds}s"""
+        if n.deviceMetrics.batteryLevel and n.deviceMetrics.voltage:
+            reply += f"Batt.: {n.deviceMetrics.batteryLevel}% {n.deviceMetrics.voltage:.3}V\n"
+        if n.deviceMetrics.channelUtilization and n.deviceMetrics.airUtilTx:
+            reply += f"Util.: {n.deviceMetrics.channelUtilization} ch, {n.deviceMetrics.airUtilTx:.2} air\n"
+        if n.deviceMetrics.uptimeSeconds:
+            reply += f"Up: {format_time(n.deviceMetrics.uptimeSeconds)}"
 
-    return reply
+    # In case we go over the size limit, truncate response
+    return reply[:200]
+
+
+def format_time(seconds: int) -> str:    
+    if seconds < 60:    
+        return f"{seconds} seconds"    
+    elif seconds < 3600:    
+        minutes = int(seconds // 60)    
+        return f"{minutes} minute{'s' if minutes > 1 else ''}"    
+    elif seconds < 86400:    
+        hours = int(seconds // 3600)    
+        return f"{hours} hour{'s' if hours > 1 else ''}"    
+    else:    
+        days = int(seconds // 86400)    
+        return f"{days} day{'s' if days > 1 else ''}"
 
 
 class NodeQuery(BaseCommand):
     command = "node"
-    description = "read from my device node DB"
-    help = "'node' for list\n'node <!id>' for detail\n'node me' for yours\n'node you' for mine"
+    description = "Read from my device node DB"
+    #help = "'node' for list\n'node <!id>' for detail\n'node me' for yours\n'node you' for mine"
+    help = ("'node' or 'node id' for NodeID list\n'node sn' for short name list\n"
+            "'node ln' for long names\n"
+            "'node <!id>' or 'node <name> for detail\n'node me' for yours\n'node you' for mine")
 
     node_list_count: int = 5
 
@@ -56,7 +90,7 @@ class NodeQuery(BaseCommand):
         msg = msg[len(self.command) :].lstrip()
 
         # they want a list
-        if msg == "":
+        if msg in ["", "id", "sn", "ln"]:
             n: NodeInfo
             ns: list[NodeInfo] = []
 
@@ -66,7 +100,7 @@ class NodeQuery(BaseCommand):
                 if n.user.id == self.interface.getMyUser()["id"]:
                     continue
                 ns.append(n)
-            return format_node_list(ns)
+            return format_node_list(ns, msg)
 
         # they want to know about themselves
         elif msg.strip().lower() == "me":
@@ -78,7 +112,16 @@ class NodeQuery(BaseCommand):
             n = NodeInfo(**self.interface.getMyNodeInfo())
             return format_node_detail(n)
 
-        # they want information about someone else
+        # they want to know about a short name
+        elif msg.strip().lower()[:1] != "!":
+            n: NodeInfo
+            for n in self.interface.nodes.values():
+                n = NodeInfo(**n)
+                if n.user.shortName.strip().lower() == msg.strip().lower():
+                    return format_node_detail(n)
+            return "I can't find that node. Use the short name or the hex identifier that begins with '!'."
+
+        # they want information about a node ID
         else:
             try:
                 n: NodeInfo = self.get_node(msg)
@@ -88,4 +131,4 @@ class NodeQuery(BaseCommand):
             if n:
                 return format_node_detail(n)
             else:
-                return "I can't find that node. Use the hex identifier that begins with '!'."
+                return "I can't find that node. Use the short name or the hex identifier that begins with '!'."
