3c25785a054145a238579416ba8ea33c6d59d6e7
Mike Krzywonski <mike@krzywonski.me>
2024-11-15 22:04:22 -0600

    Enhanced the 'ping' command to return hops, snr, and rssi to user instead of 'pong'.

--- door/commands/ping.py
+++ door/commands/ping.py
@@ -1,10 +1,23 @@
 from . import BaseCommand
 
-
 class Ping(BaseCommand):
     command = "ping"
-    description = "'ping' replies with 'pong'"
+    description = "'ping' replies with signal strength data"
     help = "This could be useful to test connectivity."
 
-    def invoke(self, msg: str, node: str) -> str:
-        return "pong"
+    def invoke(self, msg: str, node: str, packet) -> str:
+        snr = packet.get('rxSnr', None)
+        rssi = packet.get('rxRssi', None)
+        hopStart = packet.get('hopStart', None)
+        hopLimit = packet.get('hopLimit', None)
+
+        response = f"Received ping from node {node}\n"
+        if hopStart and hopLimit:
+            hops = packet['hopStart'] - packet['hopLimit']
+            response += f"Hops: {hops}\n"
+        if snr:
+            response += f"SNR: {snr}\n"
+        if rssi:
+            response += f"RSSI: {rssi}"
+
+        return response
